5846b0ed83a976cd19bfdfb097bd52cfb448a686
Jess Rezac <jess.rezac@gmail.com>
2019-07-26 16:39:40 +0000

    Automatically backed up by Learn

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_07_26_162845) do
+ActiveRecord::Schema.define(version: 2019_07_26_163911) do
 
   create_table "shows", force: :cascade do |t|
     t.string "name"
     t.string "network"
     t.string "day"
     t.integer "rating"
+    t.string "season"
   end
 
 end
