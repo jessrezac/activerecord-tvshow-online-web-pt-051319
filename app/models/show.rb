bd3c5e7067777523f18e6fca9193b35576699070
Jess Rezac <jess.rezac@gmail.com>
2019-07-26 16:42:37 +0000

    Automatically backed up by Learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -2,4 +2,12 @@ class Show < ActiveRecord::Base
   def self.highest_rating
     self.maximum(:rating)
   end
+
+  def self.most_popular_show
+    self.highest_rating
+  end
+
+  def self.lowest_rating
+    self.minimum(:rating)
+  end
 end
